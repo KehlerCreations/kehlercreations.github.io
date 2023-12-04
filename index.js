e9588d029b1a545dec260372e974208f5820726e
Elliot Kehler <admin@kehlercreations.com>
2023-12-04 19:42:55 +0100

    Began restructuring the website

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ header.innerHTML = `
 <a class="header-title" href="/"><h1 class="header-title">ELLIOT KEHLER</h1></a>
 <nav class="navigation navigation-pages">
     <ul class="main-navigation-buttons">
-        <li><a class="main-navigation-button" href="/">GAME DESIGN</a></li>
+        <li><a class="main-navigation-button" href="/">HOME</a></li>
+        <li><a class="main-navigation-button" href="game-design.html">GAME DESIGN</a></li>
         <li><a class="main-navigation-button" href="3d.html">3D DESIGN</a></li>
+        <li><a class="main-navigation-button" href="music.html">MUSIC</a></li>
         <li><a class="main-navigation-button" href="about.html">ABOUT ME</a></li>
         <li><a class="main-navigation-button" href="contact.html">CONTACT</a></li>
     </ul>
