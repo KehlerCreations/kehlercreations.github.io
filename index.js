c69c260c5973b7909c6323b75ebcc93ffcd8785a
KehlerCreations <admin@kehlercreations.com>
2023-12-06 12:45:05 +0100

    More developed About page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// HEADER
 let header = document.querySelector("header");
 header.innerHTML = `
 <span>
@@ -14,4 +15,33 @@ header.innerHTML = `
         <li><a class="main-navigation-button" href="contact.html">CONTACT</a></li>
     </ul>
 </nav>
-`
+`;
+
+// FOOTER
+let footer = document.querySelector("footer");
+footer.innerHTML = `
+<div class="contact-mail-list">
+    <a class="contact-mail" href="mailto:elliot.kehler@kehlercreations.com">elliot.kehler@kehlercreations.com</a>
+    <br>
+    <a class="contact-mail" href="mailto:elliot.kehler@gmail.com">elliot.kehler@gmail.com</a>
+</div>
+<div class="contact-links">
+    <div>
+        <a class="contact-link-link" href="https://www.discord.com" target="_blank" aria-label="Find me on Discord">
+            <img class="contact-link-image" src="Images/link_discord.png" alt="Discord logo">
+        </a>
+        
+        <a class="contact-link-link" href="https://www.linkedin.com" target="_blank" aria-label="Find me on LinkedIn">
+            <img class="contact-link-image" src="Images/link_linkedin.png" alt="LinkedIn logo">
+        </a>
+    </div>
+    <div>
+        <a class="contact-link-link" href="https://www.twitter.com" target="_blank" aria-label="Find me on X / Twitter">
+            <img class="contact-link-image" src="Images/link_twitter.png" alt="Twitter logo">
+        </a>
+        <a class="contact-link-link" href="https://www.youtube.com" target="_blank" aria-label="Find me on YouTube">
+            <img class="contact-link-image" src="Images/link_youtube.png" alt="YouTube logo">
+        </a>
+    </div>
+</div>
+`;
