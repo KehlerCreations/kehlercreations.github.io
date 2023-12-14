1dc3c96b856d6d96070b1b01c1410e24c0a1b96d
Elliot Kehler <admin@kehlercreations.com>
2023-12-14 17:50:13 +0100

    Fixed some broken links

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ header.innerHTML = `
     <ul class="main-navigation-buttons mobile">
         <li><a class="main-navigation-button mobile" href="/">HOME</a></li>
         <li><a class="main-navigation-button mobile" href="/game-design/">GAME DESIGN</a></li>
-        <li><a class="main-navigation-button mobile" href="/3d/">3D DESIGN</a></li>
+        <li><a class="main-navigation-button mobile" href="/3d-design/">3D DESIGN</a></li>
         <li><a class="main-navigation-button mobile" href="/music/">MUSIC</a></li>
         <li><a class="main-navigation-button mobile" href="/about/">ABOUT ME</a></li>
         <li><a class="main-navigation-button mobile" href="/contact/">CONTACT</a></li>
@@ -37,35 +37,9 @@ document.querySelector("#navigation-folderButton-mobile").addEventListener("clic
     let navDisplay = ID.style.display;
     if(navDisplay == "block") {
         ID.style.display = "none";
-
-        /// Change folder symbol
-        let folderID = document.getElementById("navigation-folderButton-mobile");
-        folderID.style.top = "8px";
-
-        let folderList = document.getElementsByClassName("navigation-folderButton-mobile-component");
-        folderList[0].style.transform = "";
-        folderList[0].style.position = "";
-        folderList[0].style.transform = "";
-        folderList[1].style.display = "";
-        folderList[2].style.transform = "";
-        folderList[2].style.position = "";
-
     } else {
-        /// Reveal list
         ID.style.display = "block";
         ID.classList.add("animate-reveal");
-        
-        /// Change folder symbol
-        let folderID = document.getElementById("navigation-folderButton-mobile");
-        folderID.style.top = "16px";
-
-        let folderComponentList = document.getElementsByClassName("navigation-folderButton-mobile-component");
-        folderComponentList[0].style.transform = "rotate(45deg)";
-        folderComponentList[0].style.position = "absolute";
-        folderComponentList[1].style.display = "none";
-        folderComponentList[2].style.transform = "rotate(315deg)";
-        folderComponentList[2].style.position = "absolute";
-
     }
     
 });
@@ -79,18 +53,22 @@ footer.innerHTML = `
     <a class="contact-mail" href="mailto:elliot.kehler@gmail.com">elliot.kehler@gmail.com</a>
 </div>
 <div class="contact-links">
-    <a class="contact-link-link" href="https://www.discord.com" target="_blank" aria-label="Find me on Discord">
-        <img class="contact-link-image" src="/Images/link_discord.png" alt="Discord logo">
-    </a>
-    
-    <a class="contact-link-link" href="https://www.linkedin.com" target="_blank" aria-label="Find me on LinkedIn">
-        <img class="contact-link-image" src="/Images/link_linkedin.png" alt="LinkedIn logo">
-    </a>
-    <a class="contact-link-link" href="https://www.twitter.com" target="_blank" aria-label="Find me on X / Twitter">
-        <img class="contact-link-image" src="/Images/link_twitter.png" alt="Twitter logo">
-    </a>
-    <a class="contact-link-link" href="https://www.youtube.com" target="_blank" aria-label="Find me on YouTube">
-        <img class="contact-link-image" src="/Images/link_youtube.png" alt="YouTube logo">
-    </a>
+    <div>
+        <a class="contact-link-link" href="https://www.discord.com" target="_blank" aria-label="Find me on Discord">
+            <img class="contact-link-image" src="Images/link_discord.png" alt="Discord logo">
+        </a>
+        
+        <a class="contact-link-link" href="https://www.linkedin.com" target="_blank" aria-label="Find me on LinkedIn">
+            <img class="contact-link-image" src="Images/link_linkedin.png" alt="LinkedIn logo">
+        </a>
+    </div>
+    <div>
+        <a class="contact-link-link" href="https://www.twitter.com" target="_blank" aria-label="Find me on X / Twitter">
+            <img class="contact-link-image" src="Images/link_twitter.png" alt="Twitter logo">
+        </a>
+        <a class="contact-link-link" href="https://www.youtube.com" target="_blank" aria-label="Find me on YouTube">
+            <img class="contact-link-image" src="Images/link_youtube.png" alt="YouTube logo">
+        </a>
+    </div>
 </div>
 `;
