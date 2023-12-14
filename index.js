d1cd8a52c40d1bf373971149be74627ebb75a695
KehlerCreations <admin@kehlercreations.com>
2023-12-14 15:28:38 +0100

    More mobile work

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ document.querySelector("#navigation-folderButton-mobile").addEventListener("clic
         /// Reveal list
         ID.style.display = "block";
         ID.classList.add("animate-reveal");
+        
+        // document.getElementsByClassName("main-navigation-button mobile")[0].classList.add("scale-animated");
 
         /// Change folder symbol
         let folderID = document.getElementById("navigation-folderButton-mobile");
