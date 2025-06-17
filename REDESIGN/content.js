74588898c42f30cb753666f791f0d11e85e2fffd
Elliot Kehler <admin@kehlercreations.com>
2025-06-17 10:53:27 +0200

    More portfolio work

--- REDESIGN/content.js
+++ REDESIGN/content.js
@@ -15,16 +15,35 @@ fetch("header.html")
 
 const mainGrid = document.getElementById("main-content-grid");
 const urls = ["experience.html", "skills.html", "samples.html"];
+
 // Make content load one by one with slight animation
-urls.forEach(element => {
-  const url = element;
+
+const LoadContent = function() {
+  const url = urls[currentContent];
   fetch(url)
     .then((response) => response.text())
     .then((text) => new DOMParser().parseFromString(text, "text/html"))
     .then((dom) => dom.getElementsByClassName("grid-content")[0])
     .then((content) => {
       mainGrid.append(content);
+      content.style.position = "relative";
+      content.style.top = "-100px";
+      content.style.opacity = "0";
+
+      content.style.transition = "top 1s, opacity 1s";
+      setTimeout(()=>{
+        content.style.top = "0px";
+        content.style.opacity = "1";
+      }, 5);
   });
-});
+
+  currentContent++;
+  if(currentContent >= urls.length) clearInterval(contentTimer);
+}
+
+let currentContent = 0;
+let contentTimer = setInterval(LoadContent, 100);
+
+
 
 // #endregion
