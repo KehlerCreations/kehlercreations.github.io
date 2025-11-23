18f60d9cc19f45390506d8658908ace99344ccb6
Elliot Kehler <admin@kehlercreations.com>
2025-11-23 15:30:45 +0100

    Form test

--- generate_header_footer.js
+++ generate_header_footer.js
@@ -1,8 +1,15 @@
 const header = document.getElementById("main-header");
-fetch("header.html")
+fetch("/header.html")
   .then((response) => response.text())
   .then((text) => new DOMParser().parseFromString(text, "text/html"))
   .then((dom) => dom.getElementById("header-main-bar"))
   .then((content) => {
     header.append(content);
 });
+
+setTimeout(()=>{
+  let waitingElements = document.querySelectorAll(".wait-for-header");
+  waitingElements.forEach(element => {
+    element.classList.remove("wait-for-header");
+  });
+}, 20)
