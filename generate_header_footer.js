dc93a6ba678d091f866929d8691e04937094baa1
KehlerCreations <admin@kehlercreations.com>
2025-12-11 11:59:16 +0100

    Mobile support

--- generate_header_footer.js
+++ generate_header_footer.js
@@ -7,6 +7,14 @@ fetch("/header.html")
     header.append(content);
 });
 
+fetch("/header.html")
+  .then((response) => response.text())
+  .then((text) => new DOMParser().parseFromString(text, "text/html"))
+  .then((dom) => dom.getElementById("mobile-reveal-header"))
+  .then((content) => {
+    header.append(content);
+});
+
 const footer = document.getElementById("main-footer");
 fetch("/footer.html")
   .then((response) => response.text())
@@ -22,3 +30,20 @@ setTimeout(()=>{
     element.classList.remove("wait-for-header");
   });
 }, 20)
+
+// Interactive mobile button
+setTimeout(() => {
+  let button = document.getElementById("mobile-reveal-header");
+  button.activated = false;
+  button.addEventListener("click", () => {
+    let headerMainBar = document.getElementById("header-main-bar");
+    if(button.activated) {
+      button.activated = false;
+      headerMainBar.style.display = "none";
+    } else {
+      button.activated = true;
+      headerMainBar.style.display = "grid";
+    }
+    
+  })
+}, 50)
